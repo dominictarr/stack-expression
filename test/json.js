22c3ef6ea09d76019f88698c5cf1bc639b723213
Dominic Tarr <dominic.tarr@gmail.com>
2019-09-30 12:54:30 +1300

    better tests for json example

--- test/json.js
+++ test/json.js
@@ -3,6 +3,19 @@ var json = require('../examples/json')
 var getError = require('../').getError
 // --------------------
 
+var inputs = [
+  [1],
+  [1,2,3],
+  [],
+  {},
+  1.2,
+  100,
+  1e+300,
+  1.32e-123,
+  {foo: 'bar'},
+//  '"{\"foo\":\"bar\"}"'
+]
+
 console.log(
   json(JSON.stringify({okay: [1,2,3], empty: {}}), 0).groups[0]
 )
@@ -13,7 +26,7 @@ var pkg = JSON.stringify(require('../package.json'))//fs.readFileSync(path.join(
 var pkg = fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8')
 //console.log(pkg)
 var start = Date.now()
-var N = 1000, custom, builtin
+var N = 10, custom, builtin
 for(var i = 0; i < N; i++)
   var parsed = json(pkg, 0).groups[0]
 
@@ -40,9 +53,22 @@ for(var i = 0; i < 10; i++) {
   try {
     var v = json(partial, 0)
   } catch (err) {
-//    console.log(err)
-//    console.log(partial)
-//    console.log('------')
+    //NOTE: _sometimes_ this fails to error, somehow the string is still valid.
+    if(v != null)
+      console.log('INPUT:', partial)
     assert.equal(v, null)
   }
 }
+
+//  assert.deepEqual(json('[ ]', 0).groups[0], [])
+
+
+for(var k in inputs) {
+  var str = JSON.stringify(inputs[k])
+  console.log('string:', str)
+  assert.deepEqual(json(str, 0).groups[0], inputs[k])
+  assert.deepEqual(json(JSON.stringify(inputs[k], null, 2), 0).groups[0], inputs[k])
+}
+  var str = JSON.stringify(inputs)
+  console.log('string:', str)
+  assert.deepEqual(json(str, 0).groups[0], inputs)
