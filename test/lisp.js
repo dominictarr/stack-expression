f54b28b9c866bc11bebb6fa88d29917e087c2585
Dominic Tarr <dominic.tarr@gmail.com>
2020-03-22 07:30:38 +1300

    test lisp symbols, need to write own equals assert to check types though

--- test/lisp.js
+++ test/lisp.js
@@ -1,13 +1,30 @@
 var assert = require('assert')
 var lisp = require('../examples/lisp')
 
+var isArray = Array.isArray
+
+function equals(actual, expected) {
+  if(actual === expected) return
+  if(isArray(expected))
+    assert.equal(isArray(actual), true, 'expected :'+JSON.stringify(actual) + 'to be an array')
+  if('symbol' === typeof expected)
+    return assert.equal(typeof actual, 'symbol') && assert.equal(String(actual), String(expected))
+  assert.equal(actual.length, expected.length)
+  for(var i = 0; i < expected.length; i++)
+    equals(actual[i], expected[i])
+}
+
 var input = [
   '()',
   '((()))',
   '123',
   '(1 2 3)',
   '(1 (2 (3)))',
-  '(\n\n\n)'
+  '(\n\n\n)',
+  '"hello world"',
+  '("hello world")',
+  'list',
+  '(list 1 2 3 4)'
 ]
 var output = [
   [],
@@ -15,13 +32,19 @@ var output = [
   123,
   [1,2,3],
   [1, [2, [3]]],
-  []
+  [],
+  "hello world",
+  ["hello world"],
+  Symbol('list'),
+  [Symbol('list'), 1, 2, 3, 4]
 ]
 
 //console.log(lisp('((()))', 0).groups[0])
 //console.log(lisp('"hello"', 0).groups[0])
 
 for(var i = 0; i < input.length; i++) {
-  var parsed = lisp(input[i], 0).groups[0]
-  assert.deepEqual(parsed, output[i])
+  var parsed = lisp(input[i], 0)
+  console.error(parsed)
+  var g = parsed.groups[0]
+  equals(g, output[i])
 }
